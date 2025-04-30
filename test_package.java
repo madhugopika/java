927a34e133574e91c325bae1d12f3a6f80d49b4d
madhugopika <m.mca24@mbits.ac.in>
2025-04-30 14:37:30 +0530

    Add files via upload

---
+++ test_package.java
@@ -0,0 +1,17 @@
+
+import Graphics.Area;
+public class test_package{
+public static void main(String[] args)
+{
+Area A=new Area();
+System.out.println("**COMPUTATION OF AREA OF DIFFERENT FIGURES**");
+System.out.println("\n____RECTANGLE____"); 
+A.rectangle();
+System.out.println("\n____TRIANGLE____"); 
+A.triangle();
+System.out.println("\n____SQUARE____"); 
+A.square();
+System.out.println("\n____CIRCLE____"); 
+A.circle();
+}
+}
